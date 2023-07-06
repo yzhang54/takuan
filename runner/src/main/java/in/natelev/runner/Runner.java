5e05b4d9a8e739663f9bde1ba23ef7d5735442a8
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2023-07-06 16:12:23 -0400

    ✨ Runner will re-use JUnit runner for same class tests
    Code quality has majorly decreased, but I don't want to make an abstraction
    that we don't yet need.

--- runner/src/main/java/in/natelev/runner/Runner.java
+++ runner/src/main/java/in/natelev/runner/Runner.java
@@ -1,10 +1,13 @@
 package in.natelev.runner;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
+import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Request;
 import org.junit.runner.Result;
+import org.junit.runner.manipulation.Filter;
 import org.junit.runner.notification.Failure;
 
 public class Runner {
@@ -26,30 +29,109 @@ public class Runner {
     public static void main(String[] args) throws ClassNotFoundException {
         JUnitCore junit = new JUnitCore();
         int exitCode = 0;
-        for (String arg : args) {
+
+        if (args.length == 0) {
+            System.err.println(RED + "No tests specified" + RESET);
+            System.exit(1);
+        } else if (args.length == 1) {
+            TestArg test = new TestArg(args[0]);
+            System.out.println(YELLOW + "Running " + test + RESET);
+            Result result = junit.run(Request.method(test.getTestClass(), test.getMethod()));
+            exitCode = printResult(result, test.toString());
+        } else if (args.length == 2) {
+            TestArg test1 = new TestArg(args[0]);
+            TestArg test2 = new TestArg(args[1]);
+            if (test1.getClassName().equals(test2.getClassName())) {
+                // run in same Runner
+                System.out.println(YELLOW + "Running " + test1 + " and " + test2 + RESET);
+                Request request = Request.aClass(test1.getTestClass()).filterWith(new Filter() {
+                    @Override
+                    public boolean shouldRun(Description description) {
+                        return test1.getMethod().equals(description.getMethodName())
+                                || test2.getMethod().equals(description.getMethodName());
+                    }
+
+                    @Override
+                    public String describe() {
+                        return "Only run polluter and victim";
+                    }
+                }).sortWith(new Comparator<Description>() {
+                    @Override
+                    public int compare(Description des1, Description des2) {
+                        if (des1.getMethodName().equals(test1.getMethod())) {
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    }
+                });
+                Result result = junit.run(request);
+                exitCode = printResult(result, test1.toString() + " and " + test2.toString());
+            } else {
+                // running separately is fine
+                System.out.println(YELLOW + "Running " + test1 + RESET);
+                Result result = junit.run(Request.method(test1.getTestClass(), test1.getMethod()));
+                exitCode = printResult(result, test1.toString());
+                if (exitCode == 0) {
+                    System.out.println(YELLOW + "Running " + test1 + RESET);
+                    Result result2 = junit.run(Request.method(test2.getTestClass(), test2.getMethod()));
+                    exitCode = printResult(result2, test2.toString());
+                }
+            }
+        } else {
+            System.err.println(RED + "Can't specify more than 2 tests." + RESET);
+            exitCode = 1;
+        }
+        System.exit(exitCode);
+    }
+
+    private static int printResult(Result result, String testDesc) {
+        if (result.wasSuccessful()) {
+            System.out.println(GREEN + "✓ " + testDesc + " passed" + RESET);
+            return 0;
+        } else {
+            System.out.println(RED + "\u2717 " + testDesc + " failed" + RESET);
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.getTestHeader());
+                System.out.println(failure.getMessage() + " " + failure.getDescription());
+                failure.getException().printStackTrace();
+            }
+            return 1;
+        }
+    }
+
+    private static class TestArg {
+        private String method;
+        private String className;
+        private Class<?> clazz;
+
+        private TestArg(String arg) throws ClassNotFoundException {
             // each arg must be in the form com.example.ClassName.methodName
             String[] parts = arg.split("\\.");
             if (parts.length < 2) {
                 System.out.println("Invalid argument: " + arg);
                 System.exit(1);
             }
-            String method = parts[parts.length - 1];
-            String clazzName = String.join(".", Arrays.copyOfRange(parts, 0, parts.length - 1));
-            System.out.println(YELLOW + "Running " + clazzName + "#" + method + RESET);
-            Class<?> clazz = Class.forName(clazzName);
-            Result result = junit.run(Request.method(clazz, method));
-            if (result.wasSuccessful()) {
-                System.out.println(GREEN + "✓ " + arg + " passed" + RESET);
-            } else {
-                System.out.println(RED + "\u2717 " + arg + " failed" + RESET);
-                exitCode = 1;
-                for (Failure failure : result.getFailures()) {
-                    System.out.println(failure.getTestHeader());
-                    System.out.println(failure.getMessage() + " " + failure.getDescription());
-                    failure.getException().printStackTrace();
-                }
-            }
+            method = parts[parts.length - 1];
+            className = String.join(".", Arrays.copyOfRange(parts, 0, parts.length - 1));
+            clazz = Class.forName(className);
         }
-        System.exit(exitCode);
+
+        public String getMethod() {
+            return method;
+        }
+
+        public Class<?> getTestClass() {
+            return clazz;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        @Override
+        public String toString() {
+            return className + "#" + method;
+        }
     }
 }
