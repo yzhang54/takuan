c73c06b9a8777473b857c4461ede026789327abc
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2023-07-05 22:36:27 -0400

    ✨ Support NO_DYNCOMP environment variable

--- daikon-gen-victim-polluter.sh
+++ daikon-gen-victim-polluter.sh
@@ -1,37 +1,51 @@
 #!/usr/bin/env bash
 
+if [[ -z "${NO_DYNCOMP}" ]]; then
+    COMPARABILITY_FILE='--comparability-file=Runner.decls-DynComp'
+else
+    COMPARABILITY_FILE=''
+fi
+
 run_daikon() {
     local TYPE=$1
     local SHOULD=$2
     shift; shift
 
     if [ "$SHOULD" = "pass" ]; then
-        # TODO: we should retry if it fails, but only if it fails for an actual reason, not because of the `extends` bug in DynComp
-        java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:./runner-1.0-SNAPSHOT.jar" daikon.DynComp --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' in.natelev.runner.Runner $@
+        if [[ -z "${NO_DYNCOMP}" ]]; then
+            # TODO: we should retry if it fails, but only if it fails for an actual reason, not because of the `extends` bug in DynComp
+            java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:$(dirname "$0")/runner-1.0-SNAPSHOT.jar" daikon.DynComp --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' in.natelev.runner.Runner $@
+        fi
 
-        while ! java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:./runner-1.0-SNAPSHOT.jar" daikon.Chicory --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' --comparability-file=JUnitCore.decls-DynComp in.natelev.runner.Runner $@
+        while ! java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:$(dirname "$0")/runner-1.0-SNAPSHOT.jar" daikon.Chicory --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' $COMPARABILITY_FILE in.natelev.runner.Runner $@
         do echo "Re-trying daikon.Chicory because the test(s) failed..."; sleep 1; done;
     else 
-        while java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:./runner-1.0-SNAPSHOT.jar" daikon.DynComp --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' in.natelev.runner.Runner $@
-        do echo "Re-trying daikon.DynComp because the test(s) passed..."; sleep 1; done;
+        if [[ -z "${NO_DYNCOMP}" ]]; then
+            while java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:$(dirname "$0")/runner-1.0-SNAPSHOT.jar" daikon.DynComp --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' in.natelev.runner.Runner $@
+            do echo "Re-trying daikon.DynComp because the test(s) passed..."; sleep 1; done;
+        fi
 
-        while java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:./runner-1.0-SNAPSHOT.jar" daikon.Chicory --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' --comparability-file=JUnitCore.decls-DynComp in.natelev.runner.Runner $@
+        while java -cp "./target/dependency/*:./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:$(dirname "$0")/runner-1.0-SNAPSHOT.jar" daikon.Chicory --ppt-omit-pattern='org.junit|junit.framework|junit.runner|com.sun.proxy|javax.servlet|org.hamcrest|in.natelev.runner' $COMPARABILITY_FILE in.natelev.runner.Runner $@
         do echo "Re-trying daikon.Chicory because the test(s) passed..."; sleep 1; done;
     fi
 
     echo "Running Daikon..."
 
-    java -XX:MaxRAMPercentage=50.0 -cp $DAIKONDIR/daikon.jar daikon.Daikon JUnitCore.dtrace.gz >"daikon-$TYPE.log"
+    java -XX:MaxRAMPercentage=50.0 -cp $DAIKONDIR/daikon.jar daikon.Daikon Runner.dtrace.gz >"daikon-$TYPE.log"
 
-    mv JUnitCore.inv.gz "daikon-$TYPE.inv.gz"
+    mv Runner.inv.gz "daikon-$TYPE.inv.gz"
 
     gunzip "daikon-$TYPE.inv.gz"
+
+    rm Runner.dtrace.gz
+    [[ -z "${NO_DYNCOMP}" ]] && rm Runner.decls-DynComp
 
     echo "Completed '$TYPE'. Daikon text output is in daikon-$TYPE.log, binary output is in daikon-$TYPE.inv"
 }
 
 if [ "$1" = "" ] || [ "$2" = "" ]; then
     echo "Usage: ./daikon-victim-polluter.sh <com.example.VictimTest> <com.example.PolluterTest>"
+    echo "If you are having problems during the DynComp stage, try setting the NO_DYNCOMP env variable to true."
 elif [ "$1" = "test" ]; then
     shift
     VICTIM=$1
