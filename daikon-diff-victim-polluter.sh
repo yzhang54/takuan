9d6df41a272d90838603d4ba022251ae88fdedca
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2023-08-10 12:48:23 -0400

    Use more memory when diffing

--- daikon-diff-victim-polluter.sh
+++ daikon-diff-victim-polluter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp "$(dirname "$0")/target/classes:$(dirname "$0")/daikon-5.8.16.jar" in.natelev.daikondiffvictimpolluter.DaikonDiffVictimPolluter daikon-pv.inv daikon-victim.inv daikon-polluter.inv $@
+java -cp "$(dirname "$0")/target/classes:$DAIKONDIR/daikon.jar" -Xmx6g -XX:+UseG1GC in.natelev.daikondiffvictimpolluter.DaikonDiffVictimPolluter daikon-pv.inv daikon-victim.inv daikon-polluter.inv daikon-all.inv TestMethodMap.serhm $@
